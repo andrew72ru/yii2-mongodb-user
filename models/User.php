3d6966a3c616b8fd5a9efe3131b769cf0ecc869c
Андрей Ждановских <andrew72ru@gmail.com>
2016-08-11 12:10:44 +0500

    Таймзона **форматтера** в профиле

--- models/User.php
+++ models/User.php
@@ -547,7 +547,7 @@ class User extends \yii\mongodb\ActiveRecord implements IdentityInterface
                 $this->_profile = \Yii::createObject(Profile::className());
                 $this->_profile->name = $this->username;
                 $this->_profile->user_id = $this->_id;
-                $this->_profile->timezone = \Yii::$app->getTimeZone();
+                $this->_profile->timezone = \Yii::$app->formatter->timeZone;
             }
             $this->_profile->link('user', $this);
         }
